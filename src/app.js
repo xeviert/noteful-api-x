212c7c338b004fd2e9e94a582eb5924ea08fe62b
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-01-13 19:16:48 -0600

    update cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({ origin: CLIENT_ORIGIN }))
+app.use(cors({}))
 app.use(express.json())
 
 app.use('/folders', foldersRouter)
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/noteful",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/noteful-test",
 
